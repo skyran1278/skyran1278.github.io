2f25243f3cd22382503a5b63149c2f8b72678931
skyran1278 <skyran1278@gmail.com>
2020-05-03 21:04:10 +0800

    Updates

---
+++ precache-manifest.888942e6b8d5f8ab36ffc1b3a166a83b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2589b30d603c7684f0692344b43a5ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ca168555b73cfc7582f",
+    "url": "/static/css/2.f9c3a8c9.chunk.css"
+  },
+  {
+    "revision": "5e75a4ac57271c862a1a",
+    "url": "/static/css/main.11dd55c7.chunk.css"
+  },
+  {
+    "revision": "8ca168555b73cfc7582f",
+    "url": "/static/js/2.6721e76f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6721e76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e75a4ac57271c862a1a",
+    "url": "/static/js/main.bc04bc1e.chunk.js"
+  },
+  {
+    "revision": "9892cfb2d5974283d561",
+    "url": "/static/js/runtime-main.ae7ff5eb.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
