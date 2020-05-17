142cb13082f048312ee07467134809f9a4b6b065
skyran1278 <skyran1278@gmail.com>
2020-05-17 20:46:00 +0800

    重頭開始

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>You Ran Nai</h1>
-        <p>來說個故事吧</p>
+        <h1>You-Ran Nai</h1>
+        <p>以貢獻和回饋社會為終極目標</p>
       </header>
     </div>
   );
