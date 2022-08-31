90f24fe92ff0854ca682b558c6f85cdd13a96087
ran <skyran1278@gmail.com>
2022-08-31 22:36:37 +0800

    style: run prettier

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -3,7 +3,9 @@
 /* eslint-disable promise/catch-or-return */
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals: (onPerfEntry?: ReportHandler | undefined) => void = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals: (onPerfEntry?: ReportHandler | undefined) => void = (
+  onPerfEntry?: ReportHandler
+) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
