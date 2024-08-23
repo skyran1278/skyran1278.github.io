1dc68437c12ffddec5a81dc02cac2039c8481296
skyran1278 <skyran1278@gmail.com>
2024-08-23 18:38:05 +0800

    style(prettier): update prettier configurations for better plugin compatibility
    - reorder plugins to ensure proper functionality
    - clarify comments for future reference on plugin order

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,7 +5,12 @@ module.exports = {
   plugins: [
     'prettier-plugin-embed',
     'prettier-plugin-sql',
-    'prettier-plugin-import-sort', // prettier-plugin-import-sort should be place after prettier-plugin-embed
+
+    // prettier-plugin-import-sort should be place after prettier-plugin-embed
+    'prettier-plugin-import-sort',
+
+    // https://github.com/tailwindlabs/prettier-plugin-tailwindcss?tab=readme-ov-file#compatibility-with-other-prettier-plugins
+    // MUST come last
     'prettier-plugin-tailwindcss',
   ],
   language: 'postgresql',
